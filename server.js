27393b1ef01643a37fd6a079739d43c19e6756c6
Iman Datta <dattaiman56@gmail.com>
2026-04-18 21:08:29 +0530

    Productin ready

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require ("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 const PORT = 3000;
 
-app.get("/", (req,res) => {
-    res.json({message: "Backend is working"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is working" });
+});
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
